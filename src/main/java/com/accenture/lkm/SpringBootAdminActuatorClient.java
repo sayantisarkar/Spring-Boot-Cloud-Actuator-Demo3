bf03dff92f433513d05065d3fc8c6efc1280c282
= <sayanti.a.sarkar@accenture.com>
2022-08-20 19:23:54 +0530

    SpringBootCloud-AdminActuatorClientDemo

---
+++ src/main/java/com/accenture/lkm/SpringBootAdminActuatorClient.java
@@ -0,0 +1,12 @@
+package com.accenture.lkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminActuatorClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminActuatorClient.class, args);
+    }
+}
